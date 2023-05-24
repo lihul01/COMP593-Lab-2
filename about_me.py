72f89e54fbb89c558aae434014d7026613fe23bb
lihul01 <lihul@flemingcollege.ca>
2023-05-24 18:57:19 -0400

    Finished

--- about_me.py
+++ about_me.py
@@ -18,7 +18,7 @@ def main():
         'movies': [
             {
                 'title': 'Rio',
-                'genre': 'Adventure'
+                'genre': 'Comedy'
             },
             {
                 'title': 'Atlantis: The Lost Empire',
@@ -56,8 +56,10 @@ def print_student_name_and_id(my_info):
     """
     # TODO: Complete function body per Step 3
     # Print sentence containing name
+    first_name = my_info['name'].split()
+    print(f"My name is {my_info['name']}, but you can call me Sir {first_name[0]}")
     # Print sentence containing student ID
-    print()
+    print(f"\nMy Fleming student ID is {my_info['student ID']}.")
 
 def print_pizza_toppings(my_info):
     """Prints a bullet list of favourite pizza toppings
@@ -67,8 +69,10 @@ def print_pizza_toppings(my_info):
     """
     # TODO: Complete function body per Step 4
     # Print header "My favourite pizza toppings are:"
+    print("\nMy favourite pizza toppings are:")
     # Print bullet list of favourite pizza toppings
-    print()
+    for toppings in my_info['pizza toppings']:
+        print(f"- {toppings}")
 
 def add_pizza_toppings(my_info, toppings):
     """Adds some pizza toppings to the list of favourites
@@ -79,8 +83,12 @@ def add_pizza_toppings(my_info, toppings):
     """
     # TODO: Complete function body per Step 5
     # Append new pizza toppings to end of list 
+    my_info['pizza toppings'].extend(toppings)
     # Convert all pizza toppings to lowercase
+    for i, toppings in enumerate(my_info['pizza toppings']):
+        my_info['pizza toppings'][i] = toppings.lower()
     # Sort toppings list alphabetically
+    my_info['pizza toppings'].sort()
     return
 
 def add_movie(my_info, title, genre):
@@ -93,7 +101,11 @@ def add_movie(my_info, title, genre):
     """
     # TODO: Complete function body per Step 6
     # Create dictionary for new movie and add to movie list
-    return
+    new_movie = {
+        'title': title,
+        'genre': genre
+    }
+    my_info['movies'].append(new_movie)
 
 def print_movie_genres(my_info):
     """Prints a sentence listing all favourite movie genres
@@ -102,7 +114,12 @@ def print_movie_genres(my_info):
         my_info (dict): Data structure containing information about me
     """
     # TODO: Complete function body per Step 7
-    print()
+    print('\nI like to watch ', end = '')
+    for i, movie in enumerate(my_info['movies']):
+        if i < len(my_info['movies']) - 1:
+            print(movie['genre'].title(), end = ', ')
+        else:
+            print('and ' + movie['genre'].title(), end = '.')
 
 def print_movie_titles(movie_list):
     """Prints a sentence listing all favourite movie titles
@@ -111,7 +128,18 @@ def print_movie_titles(movie_list):
         movie_list (list): List of favourite movies
     """
     # TODO: Complete function body per Step 8
-    print()
+    print('\n\nSome of my favourite movies are ', end = "")
+    x = 0
+    while x < len(movie_list):
+        movie = movie_list[x]
+        title = movie['title'].title()
+        if x < len(movie_list) - 1:
+            print(f"{title}", end = ', ')
+        else:
+            print(f"and {title}!")
+        x = x + 1
+    #titles = [title['title'] for title in movie_list['movies']]
+    #print(', '.join(titles), end='.')
 
 if __name__ == '__main__':
     main()
